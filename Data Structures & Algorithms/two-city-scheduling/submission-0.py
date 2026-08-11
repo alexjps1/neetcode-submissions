1ae084c0cf0706a09c872084d972e6dc9e0e0349
Alexander João Peterson Santos <github@alexjps.com>
2026-08-11 19:47:10 +0200

    Add: two-city-scheduling - submission-0

---
+++ Data Structures & Algorithms/two-city-scheduling/submission-0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        costs.sort(key=lambda x: x[0] - x[1])
+        n = len(costs) // 2
+        total = sum(cost[0] for cost in costs[:n]) + sum(cost[1] for cost in costs[n:])
+        return total
